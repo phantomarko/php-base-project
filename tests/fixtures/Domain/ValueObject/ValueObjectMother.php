1479f34690955c28904dd38813e1dc6edcccf7b7
phantomarko <phantomarko@gmail.com>
2024-01-04 14:09:43 +0100

    fix: set VO mother methods parameters as optional

--- tests/fixtures/Domain/ValueObject/ValueObjectMother.php
+++ tests/fixtures/Domain/ValueObject/ValueObjectMother.php
@@ -12,12 +12,12 @@ final class ValueObjectMother
 {
     use StringHelper;
 
-    public static function makeUuid(?string $value): Uuid
+    public static function makeUuid(?string $value = null): Uuid
     {
         return new Uuid(is_null($value) ? self::generateUuidRfc4122() : $value);
     }
 
-    public static function makeId(?int $value): Id
+    public static function makeId(?int $value = null): Id
     {
         return new Id(is_null($value) ? rand(Id::MIN_VALUE, Id::MAX_VALUE) : $value);
     }
